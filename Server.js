3bd123f47d5cc8c7f425d730ceec07d86e365a78
Sabiya <sabiya.sachdeva@gmail.com>
2026-01-18 16:14:02 -0500

    Adding server component

---
+++ Server.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+import router from "./router/auth-router.js";
+import connectDb from "./Database/Db.js";
+import cors from "cors";
+const app = express();
+
+app.use(cors());
+const port = 3001;
+app.use(express.json());
+app.use("/api", router);
+
+app.use(express.static("public"));
+
+const start = async () => {
+  try {
+    await connectDb();
+    app.listen(port, () => {
+      console.log(`server has started and is listening on port number:${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
