c4c0c5c0874da11913d2a853cf2cd67a4a84f31d
Sabiya <sabiya.sachdeva@gmail.com>
2026-03-11 22:56:35 -0400

    added search function

--- src/DiseasesandCondition/DiseasesandCondition.js
+++ src/DiseasesandCondition/DiseasesandCondition.js
@@ -18,6 +18,7 @@ function DiseaseandCondition() {
     navigate(`/diseases/${id}`);
   };
   const [cardData, setCardData] = useState([]);
+  const [search, setSearch] = useState("");
   useEffect(() => {
     const fetchDisease = async () => {
       try {
@@ -32,43 +33,76 @@ function DiseaseandCondition() {
     };
     fetchDisease();
   }, []);
- 
+
+  const filteredDisease = cardData.filter((item) =>
+    item.tittle.toLowerCase().includes(search.toLowerCase()),
+  );
 
   return (
     <div>
-      <Navbar/>
-      <Toolbar/>
-      <Typography variant="h2" align="center" sx={{mt:5}}>
+      <Navbar />
+      <Toolbar />
+      <Typography variant="h2" align="center" sx={{ mt: 5 }}>
         Diseases And Condition
       </Typography>
-      <Typography variant="subtitle1" align="center" gutterBottom sx={{mb:5}}>
+      <Typography
+        variant="subtitle1"
+        align="center"
+        gutterBottom
+        sx={{ mb: 5 }}
+      >
         Detailed Insights into Causes, Risk Factors, Preventions, and Management
       </Typography>
+      <Typography align="center" gutterBottom sx={{ mb: 5 }}>
+        <input
+          type="text"
+          value={search}
+          placeholder="Search for Disease"
+          onChange={(e) => setSearch(e.target.value)}
+          style={{ padding: "10px", width: "300px", borderRadius: "6px" }}
+        ></input>
+      </Typography>
+      <Box
+        display="flex"
+        flexWrap="wrap"
+        justifyContent="center"
+        gap={7}
+        mb={10}
+      >
+        {filteredDisease.length > 0 ? (
+          filteredDisease.map((item) => (
+            <Card
+              key={item.id}
+              sx={{ width: 390, backgroundColor: "lightblue" }}
+            >
+              <CardMedia
+                component="img"
+                height="150"
+                image={item.image}
+                alt=""
+              ></CardMedia>
+              <CardContent>
+                <Typography variant="h5">{item.tittle}</Typography>
+              </CardContent>
 
-      <Box display="flex" flexWrap="wrap" justifyContent="center" gap={7} mb={10}>
-        {cardData.map((item) => (
-          <Card key={item.id} sx={{ width: 390, backgroundColor: "lightblue" }}>
-            <CardMedia
-              component="img"
-              height="150"
-              image={item.image}
-              alt=""
-            ></CardMedia>
-            <CardContent>
-              <Typography variant="h5">{item.tittle}</Typography>
-            </CardContent>
-            <CardActions>
-              <Button
-                variant="contained"
-                onClick={() => handlereadmore(item.id)}
-              >
-                Read More{" "}
-              </Button>
-            </CardActions>
-          </Card>
-        ))}
+              <CardActions>
+                <Button
+                  variant="contained"
+                  onClick={() => handlereadmore(item.id)}
+                >
+                  Read More{" "}
+                </Button>
+              </CardActions>
+            </Card>
+          ))
+        ) : (
+          <Typography variant="h6" align="center">
+            No Disease found.
+          </Typography>
+        )}
       </Box>
-      <Footerdetails/>
+
+      <Footerdetails />
     </div>
   );
 }
